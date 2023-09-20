85b5d7bedcb3e45a9a6ae3d71ab4c34b5c76e25e
Blergborg <pprater92@gmail.com>
2023-09-20 09:45:52 -0700

    Initial Commit

---
+++ CupcakeCounter/CupcakeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCounterApp.swift
+//  CupcakeCounter
+//
+//  Created by Phil Prater on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
